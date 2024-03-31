56215ee0636c57d7bdaf65cccab67878fb17d6ae
Fariha <farihasheikh529@gmail.com>
2024-03-31 11:18:15 -0700

    Q7 and Q8

---
+++ Q7 and Q8/main.js
@@ -0,0 +1,19 @@
+"use strict";
+// Excercise 7 and 8
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. 
+// Be sure to enclose your operations in print statements to see the results.
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+let a = 6;
+let b = 2;
+console.log("Addition:", a + b); //Adition of 2 numbers
+let d = 10;
+let e = 2;
+console.log("Subtraction:", d - e); //Subtraction of 2 numbers
+let g = 2;
+let h = 4;
+console.log("Multiplication:", g * h); //Multiplication of 2 numbers
+let i = 32;
+let j = 4;
+console.log("Division:", i / j); //Division of 2 numbers
