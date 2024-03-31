2b52a08041610a91f0714050e3830f8e88ee05cb
Fariha <farihasheikh529@gmail.com>
2024-03-31 11:22:43 -0700

    Q9 commit

---
+++ Q9/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Excercise 9
+// Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+const favouriteNumber = 18;
+const message = 'This is my favourite number: ' + favouriteNumber;
+console.log(message);
---
+++ Q9/main.ts
@@ -0,0 +1,12 @@
+
+
+// Excercise 9
+
+// Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+const favouriteNumber:number=18;
+
+const message:string='This is my favourite number: ' + favouriteNumber;
+
+console.log(message);
