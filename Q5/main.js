ee63800b6246f2a6c9def87acef149262049bf6f
Fariha <farihasheikh529@gmail.com>
2024-03-31 11:11:00 -0700

    Q5 commit

---
+++ Q5/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Excercise # 5
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+// Then compose your message and store it in a new variable called message. Print your message.
+let famous_person = 'Quaid-e-Azam';
+let message = '“There is no power on earth that can undo Pakistan.”';
+console.log(`${famous_person} once said, ${message}`);
