1f8675fca65c237a033f50ac09c2c74c29ef3c90
Fariha <farihasheikh529@gmail.com>
2024-03-31 11:58:15 -0700

    Q20 commit

---
+++ Q20/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Excercise 20
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities,
+//  languages, or anything else you’d like. Write a program that creates a list containing these items.
+//Think of something you could store in a array.
+let items = ['United States', 'Chicago', 'English'];
+console.log(items);
+//Write a program that creates a list containing these items.
+let item2 = [];
+item2.push('Pakistan');
+item2.push('Karachi');
+item2.push('Urdu');
+console.log(item2);
