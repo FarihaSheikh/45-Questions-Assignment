2fab211f290c9f6a71ed05ad531ad513f72515d8
Fariha <farihasheikh529@gmail.com>
2024-03-31 11:25:50 -0700

    Q10 commit

---
+++ Q10/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Program created by: Fariha Fatima
+// Current Date: 25-2-2023
+//This program will multiply 2 numbers.
+let a = 4;
+let b = 2;
+console.log("Multiplication:", a * b);
